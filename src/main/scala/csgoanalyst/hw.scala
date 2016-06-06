cdd035936425e8271d130a8e36749c365d593b14
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2016-06-06 00:20:43 -0300

    12

--- src/main/scala/csgoanalyst/hw.scala
+++ src/main/scala/csgoanalyst/hw.scala
@@ -7,14 +7,7 @@ import scala.util.Try
 
 
 object GetAttackers extends spark.jobserver.SparkJob {
-//  def main(args: Array[String]) {
-//    val conf = new SparkConf().setMaster("local[4]").setAppName("csgoanalyst")
-//    val sc = new SparkContext(conf)
-//    val config = ConfigFactory.parseString("")
-//    val results = runJob(sc, config)
-//    println(results)
-//
-//  }
+
 
   override def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = {
     spark.jobserver.SparkJobValid
@@ -26,20 +19,13 @@ object GetAttackers extends spark.jobserver.SparkJob {
 
     demo.map(line => line.split(',')).
          map(fields => (fields(9),fields(10),fields(11),fields(12),fields(13),fields(14),fields(15),fields(16),fields(17),fields(18),fields(19))).
-         collect();
+         collect().sum
    }
   
 }
 
 
 object WordCountExample extends spark.jobserver.SparkJob {
-//  def main(args: Array[String]) {
-//    val conf = new spark.jobserver.SparkConf().setMaster("local[4]").setAppName("WordCountExample")
-//    val sc = new spark.jobserver.SparkContext(conf)
-//    val config = ConfigFactory.parseString("")
-//    val results = runJob(sc, config)
-//    println("Result is " + results)
-//  }
 
   override def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = {
     Try(config.getString("input.string"))
