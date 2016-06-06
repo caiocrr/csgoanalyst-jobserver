84d13b94ec85caf412545bffef12ffe0a8783ed2
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2016-06-06 00:50:48 -0300

    546

--- src/main/scala/csgoanalyst/hw.scala
+++ src/main/scala/csgoanalyst/hw.scala
@@ -21,8 +21,10 @@ object GetAttackers extends spark.jobserver.SparkJob with spark.jobserver.NamedR
     
 //    case class Attacker(item1: String, item2: String, item3: String, item4: String, item5: String, item6: String, item7: String, item8: String, item9: String, item10: String, item11: String)
     
-    val attackers = demo.map(line => line.split(',')).map(fields => (fields(9),fields(10),fields(11),fields(12),fields(13),fields(14),fields(15),fields(16),fields(17),fields(18),fields(19))).collect()
-    val users: RDD[Array[(String, String, String, String, String, String, String, String, String, String, String)]] = namedRdds.get(attackers)
+    val attackers = demo.map(line => line.split(',')).map(fields => (fields(9),fields(10),fields(11),fields(12),fields(13),fields(14),fields(15),fields(16),fields(17),fields(18),fields(19)))
+    
+    attackers.collect().foreach(println);
+    
     
     
    }
