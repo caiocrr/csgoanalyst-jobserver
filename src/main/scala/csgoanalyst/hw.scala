5c5987527e3adcf6919daf966c932afa467c3e16
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2016-06-05 22:43:28 -0300

    656

--- src/main/scala/csgoanalyst/hw.scala
+++ src/main/scala/csgoanalyst/hw.scala
@@ -25,7 +25,7 @@ object GetAttackers extends spark.jobserver.SparkJob {
           map(line => line.split(',')).
           map(fields => (fields(9),fields(10),fields(11),fields(12),fields(13),fields(14),fields(15),fields(16),fields(17),fields(18),fields(19)))
     
-    attackersFiltered.take(1);
+    attackersFiltered.collect();
   }
 }
 
