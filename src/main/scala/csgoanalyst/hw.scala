18a4a33a1c3e8b6c7d5155442fbf7c2225291718
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2016-06-05 22:17:12 -0300

    3

--- src/main/scala/csgoanalyst/hw.scala
+++ src/main/scala/csgoanalyst/hw.scala
@@ -15,7 +15,7 @@ object GetAttackers extends spark.jobserver.SparkJob {
     println(results)
   }
 
-  def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation
+  override def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation
   
   override def runJob(sc: SparkContext, config: Config): Any = {
     val demo = sc.textFile("file:/home/caiocrr/Desktop/csgodemos/test1.txt").
