84c7b9d2446d34d0ba7bb7850f92509ebeb2ead1
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2016-06-05 23:21:59 -0300

    12

--- src/main/scala/csgoanalyst/hw.scala
+++ src/main/scala/csgoanalyst/hw.scala
@@ -10,25 +10,24 @@ object GetAttackers extends spark.jobserver.SparkJob {
     val conf = new SparkConf().setMaster("local[4]").setAppName("csgoanalyst")
     val sc = new SparkContext(conf)
     val config = ConfigFactory.parseString("")
-    runJob(sc, config)
+    val results = runJob(sc, config)
+    println(results)
 
   }
 
-  def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = spark.jobserver.SparkJobValid
+  override def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = {
+    spark.jobserver.SparkJobValid
+  }
   
   override def runJob(sc: SparkContext, config: Config): Any = {
     val demo = sc.textFile("file:/home/caiocrr/Desktop/csgodemos/test1.txt").
       filter(line => line.startsWith("victim"))
 
-    val attackersFiltered = demo.
-          map(line => line.split(',')).
-          map(fields => (fields(9),fields(10),fields(11),fields(12),fields(13),fields(14),fields(15),fields(16),fields(17),fields(18),fields(19)))
-    
-    attackersFiltered.collect().foreach(println);
-  }
+    demo.map(line => line.split(',')).
+         map(fields => (fields(9),fields(10),fields(11),fields(12),fields(13),fields(14),fields(15),fields(16),fields(17),fields(18),fields(19))).
+         collect();
+   }
 }
-
-
 
 
 
