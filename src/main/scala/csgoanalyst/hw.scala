9c22e04ecd7cda46702b3359c7c2de72359449b2
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2016-06-06 00:42:01 -0300

    45

--- src/main/scala/csgoanalyst/hw.scala
+++ src/main/scala/csgoanalyst/hw.scala
@@ -6,7 +6,7 @@ import org.apache.spark.SparkContext._
 import scala.util.Try
 
 
-object GetAttackers extends spark.jobserver.SparkJob {
+object GetAttackers extends spark.jobserver.SparkJob with spark.jobserver.NamedRddSupport {
 
 
   override def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = {
@@ -20,11 +20,10 @@ object GetAttackers extends spark.jobserver.SparkJob {
     
 //    case class Attacker(item1: String, item2: String, item3: String, item4: String, item5: String, item6: String, item7: String, item8: String, item9: String, item10: String, item11: String)
     
-    val attackers = demo.map(line => line.split(','))
-    val attackersFiltered = attackers.map(fields => (fields(9),fields(10),fields(11),fields(12),fields(13),fields(14),fields(15),fields(16),fields(17),fields(18),fields(19)))
-        
+    val attackers = demo.map(line => line.split(',')).map(fields => (fields(9),fields(10),fields(11),fields(12),fields(13),fields(14),fields(15),fields(16),fields(17),fields(18),fields(19))).collect()
+    val users: RDD[(String, String, String, String, String, String, String, String, String, String, String)] = namedRdds.get(attackers)
     
-    attackersFiltered.collect()
+    
    }
   
 }
