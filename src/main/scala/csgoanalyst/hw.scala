87a14dae7be5e096eb5f794add5cbdb918bc6ac2
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2016-06-06 01:05:22 -0300

    çj

--- src/main/scala/csgoanalyst/hw.scala
+++ src/main/scala/csgoanalyst/hw.scala
@@ -18,12 +18,11 @@ object GetAttackers extends spark.jobserver.SparkJob with spark.jobserver.NamedR
     val demo = sc.textFile("file:/home/caiocrr/Desktop/csgodemos/test1.txt").
       filter(line => line.startsWith("victim"))
 
+    case class Attacker(item1: String, item2: String, item3: String, item4: String, item5: String, item6: String, item7: String, item8: String, item9: String, item10: String, item11: String)
     
-//    case class Attacker(item1: String, item2: String, item3: String, item4: String, item5: String, item6: String, item7: String, item8: String, item9: String, item10: String, item11: String)
+    val attackers = demo.map(line => line.split(',')).map(fields => new Attacker((fields(9),fields(10),fields(11),fields(12),fields(13),fields(14),fields(15),fields(16),fields(17),fields(18),fields(19))))
     
-    val attackers = demo.map(line => line.split(',')).map(fields => (fields(9),fields(10),fields(11),fields(12),fields(13),fields(14),fields(15),fields(16),fields(17),fields(18),fields(19)))
-    
-    attackers.collect().foreach(println);
+    attackers.collect().foreach(println)
     
     
     
