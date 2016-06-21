ad3de4228d433e55ac6719040fc54ce363bf8efb
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2016-06-21 15:43:51 -0300

    -
    Atualizado ~  3 funções feitas(ListKill, TopKill e KDEspec)

--- src/main/scala/csgoanalyst/hw.scala
+++ src/main/scala/csgoanalyst/hw.scala
@@ -5,11 +5,12 @@ import org.apache.spark._
 import org.apache.spark.SparkContext._
 import scala.util.Try
 import org.apache.spark.rdd.RDD
+import scala.runtime.ScalaRunTime._
+import org.apache.spark.rdd.RDD
 import java.io.File
 
 
-
-object GetAttackers extends spark.jobserver.SparkJob with spark.jobserver.NamedRddSupport {
+object TopKillers extends spark.jobserver.SparkJob {
 
 
   override def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = {
@@ -17,34 +18,103 @@ object GetAttackers extends spark.jobserver.SparkJob with spark.jobserver.NamedR
   }
   
   override def runJob(sc: SparkContext, config: Config): Any = {
-    val demo = sc.textFile("file:/home/caiocrr/Desktop/csgodemos/test1.txt").
-      filter(line => line.startsWith("victim"))
+    
+    val qtd = config.getString("qtd").toInt
+    
+    var attackersCount: RDD[(String,Int)] = sc.emptyRDD
 
-//    case class Attacker(item1: String, item2: String, item3: String, item4: String, item5: String, item6: String, item7: String, item8: String, item9: String, item10: String, item11: String)
-    
-    val attackers = demo.map(line => line.split(',')).map(fields => Array(fields(9),fields(10),fields(11),fields(12),fields(13),fields(14),fields(15),fields(16),fields(17),fields(18),fields(19)))
-    
-    attackers.collect()   
-    
+    val demo = sc.textFile("/home/caiocrr/Desktop/demo_manager/txts/*.txt").
+    filter(line => line.startsWith("victim")).
+    filter(line => !line.contains("knife"))
+
+    val attackersName = demo.map(line => line.split(',')).map(fields => (fields(10).trim, 1))
+
+    var attackersCountDemo = attackersName.reduceByKey((v1,v2) => v1 + v2)
+    attackersCount = attackersCount.union(attackersCountDemo)
+
+    attackersCount.
+    reduceByKey((v1,v2) => v1 + v2).
+    sortBy {- _._2}.
+    take(qtd)
     
    }
   
 }
 
 
-object getDemoName extends spark.jobserver.SparkJob {
+object KillDeath extends spark.jobserver.SparkJob {
 
-   override def runJob(sc: SparkContext, config: Config): Any = {
-      val d = new File("/home/caiocrr/Desktop/csgodemos").list
-      val rddFiles = sc.parallelize(d)    
-      rddFiles.collect()
-  }
-  
-    
 
   override def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = {
     spark.jobserver.SparkJobValid
-  } 
+  }
+  
+  override def runJob(sc: SparkContext, config: Config): Any = {
+    
+    val nickname = config.getString("nickname")
+    
+    var attackersCount: RDD[(String,Int)] = sc.emptyRDD
+    var victimsCount: RDD[(String,Int)] = sc.emptyRDD
+            
+    val demo = sc.textFile("/home/caiocrr/Desktop/demo_manager/txts/*.txt").
+    filter(line => line.startsWith("victim")).
+    filter(line => !line.contains("knife"))
+
+    val attackersName = demo.map(line => line.split(',')).
+    map(fields => (fields(10).trim, 1)).
+    filter(_._1.contains(nickname))  
+
+    val victimsName = demo.map(line => line.split(',')).
+    map(fields => (fields(1).trim, 1)).
+    filter(_._1.contains(nickname))
+
+    var attackersCountDemo = attackersName.reduceByKey((v1,v2) => v1 + v2)
+    attackersCount = attackersCount.union(attackersCountDemo)
+
+    var victimsCountDemo = victimsName.reduceByKey((v1,v2) => v1 + v2)
+    victimsCount = victimsCount.union(victimsCountDemo)
+
+    val result = attackersCount.
+    reduceByKey((v1,v2) => v1 + v2).
+    join(victimsCount.
+    reduceByKey((v1,v2) => v1 + v2))
+
+    result.collect()
+    
+   }
+  
+}
+
+
+
+
+object ListPlayers extends spark.jobserver.SparkJob {
+
+
+  override def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = {
+    spark.jobserver.SparkJobValid
+  }
+  
+  override def runJob(sc: SparkContext, config: Config): Any = {
+    
+    var playersCount: RDD[(String)] = sc.emptyRDD
+     
+    val demo = sc.textFile("/home/caiocrr/Desktop/demo_manager/txts/*.txt").
+    filter(line => line.startsWith("victim"))
+
+    val playersName = demo.map(line => line.split(',')).
+    map(fields => (fields(1).trim)).distinct()
+
+    playersCount = playersCount.union(playersName)
+
+    val result = playersCount.
+    distinct()
+
+    result.sortBy(string => string).collect()
+
+    
+   }
+  
 }
 
 
