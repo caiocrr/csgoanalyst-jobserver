3aa21e501233a40778e2dca02b32df04b6fa0292
Caio César Riqueza Ramos <caiotecn@yahoo.com.br>
2016-06-21 18:46:59 -0300

    TopMvp
    nova função TopMVP

--- src/main/scala/csgoanalyst/hw.scala
+++ src/main/scala/csgoanalyst/hw.scala
@@ -42,7 +42,7 @@ object TopKillers extends spark.jobserver.SparkJob {
 }
 
 
-object KillDeath extends spark.jobserver.SparkJob {
+object KDESpec extends spark.jobserver.SparkJob {
 
 
   override def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = {
@@ -86,6 +86,47 @@ object KillDeath extends spark.jobserver.SparkJob {
 }
 
 
+object TopKD extends spark.jobserver.SparkJob {
+
+
+  override def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = {
+    spark.jobserver.SparkJobValid
+  }
+  
+  override def runJob(sc: SparkContext, config: Config): Any = {
+    
+    val qtd = config.getString("qtd").toInt
+    
+    var attackersCount: RDD[(String,Int)] = sc.emptyRDD
+    var victimsCount: RDD[(String,Int)] = sc.emptyRDD
+            
+    val demo = sc.textFile("/home/caiocrr/Desktop/demo_manager/txts/*.txt").
+    filter(line => line.startsWith("victim")).
+    filter(line => !line.contains("knife"))
+
+    val attackersName = demo.map(line => line.split(',')).
+    map(fields => (fields(10).trim, 1))
+
+    val victimsName = demo.map(line => line.split(',')).
+    map(fields => (fields(1).trim, 1))
+
+    var attackersCountDemo = attackersName.reduceByKey((v1,v2) => v1 + v2)
+    attackersCount = attackersCount.union(attackersCountDemo)
+
+    var victimsCountDemo = victimsName.reduceByKey((v1,v2) => v1 + v2)
+    victimsCount = victimsCount.union(victimsCountDemo)
+
+    val result = attackersCount.
+    reduceByKey((v1,v2) => v1 + v2).
+    join(victimsCount.
+    reduceByKey((v1,v2) => v1 + v2))
+
+    result.take(qtd)
+    
+   }
+  
+}
+
 
 
 object ListPlayers extends spark.jobserver.SparkJob {
@@ -118,5 +159,31 @@ object ListPlayers extends spark.jobserver.SparkJob {
 }
 
 
+
+
+object TopMVP extends spark.jobserver.SparkJob {
+
+
+  override def validate(sc: SparkContext, config: Config): spark.jobserver.SparkJobValidation = {
+    spark.jobserver.SparkJobValid
+  }
+  
+  override def runJob(sc: SparkContext, config: Config): Any = {
+    
+
+    val qtd = config.getString("qtd").toInt
+    
+    var demo = sc.textFile("/home/caiocrr/Desktop/demo_manager/txts/*.txt").zipWithIndex.map{case (k,v) => (v,k)}
+    var getMVPLine = demo.filter(_._2.contains("round_mvp"))
+    
+    var getMVPLineKeys = getMVPLine.keys.map(x => x+2).zipWithIndex
+
+    val playersMVP = demo.join(getMVPLineKeys).map{
+      case (x, (y,z)) => y.trim
+    }.map(x => x.split(" ")).map(x => (x(1), 1)).reduceByKey((v1,v2) => v1 + v2).sortBy {- _._2}.collect()
+     
+   }
+  
+}
 
  
